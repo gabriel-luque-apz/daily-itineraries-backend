c83c837d21c2c063cff743af1b51cb4a1d334420
Gabriel <luquegabrielies@gmail.com>
2025-11-02 14:49:47 +0100

    build(project): files project files

---
+++ src/main/java/com/gabriel/springboot/app/dailyitineraries/DailyItinerariesApplication.java
@@ -0,0 +1,13 @@
+package com.gabriel.springboot.app.dailyitineraries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyItinerariesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailyItinerariesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gabriel/springboot/app/dailyitineraries/DailyItinerariesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.springboot.app.dailyitineraries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyItinerariesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
